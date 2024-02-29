ce3eebf30e168cd3257f50958a292ec44f7a05ab
dilafar <it20784720@my.sliit.lk>
2024-03-01 04:03:03 +0530

    versioning build added

---
+++ src/com/example/Version.groovy
@@ -0,0 +1,20 @@
+package com.example
+
+class Version implements Serializable{
+    def script
+    Version(script){
+        this.script=script
+    }
+
+    def incrementMajorVersion(){
+        script.sh "mvn build-helper:parseVersion versions:set -DnewVersion=\\\${parsedVersion.nextMajorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.incrementalVersion} versions:commit"
+    }
+
+    def incrementMinorVersion(){
+        script.sh "mvn build-helper:parseVersion versions:set -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.nextMinorVersion}.\\\${parsedVersion.incrementalVersion} versions:commit"
+    }
+
+    def incrementPatchVersion(){
+        script.sh "mvn build-helper:parseVersion versions:set -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} versions:commit"
+    }
+}
---
+++ vars/incrementMinorVersion.groovy
@@ -0,0 +1,7 @@
+#!usr/bin/env groovy
+import com.example.Version
+
+
+def call(){
+    return new Version(this).incrementMinorVersion()
+}
