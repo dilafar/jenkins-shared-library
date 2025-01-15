212ea2a5c7f717c2a26b85228839a29e03eab54f
dilafar <it20784720@my.sliit.lk>
2025-01-15 13:14:29 +0530

    cosign added

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,6 +1,6 @@
 #!usr/bin/env groovy
 import com.example.Docker
 
-def call(String imageName){
-    return new Docker(this).pushDockerImage(imageName)
+def call(String imageName,String imageVersion){
+    return new Docker(this).pushDockerImage(imageName,imageVersion)
 }
