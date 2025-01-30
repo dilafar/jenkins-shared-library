a808d84a5ff7b5954d7268b3caed51ba8d0ca3a5
dilafar <it20784720@my.sliit.lk>
2025-01-30 12:27:15 +0530

    docker login private cloud added

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -27,7 +27,7 @@ class Docker implements Serializable{
 
     def dockerLoginPrivateCloud(String repoUrl){
         script.withCredentials([
-                script.usernamePassword(credentialsId: 'docker', passwordVariable: 'PASS', usernameVariable: 'USER')
+                script.usernamePassword(credentialsId: 'ecr-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')
         ]){
             script.sh "echo ${script.PASS} | docker login -u ${script.USER} --password-stdin ${repoUrl}"
 
